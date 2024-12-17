3a31b674da6c1ca2d173143f1115383aa94e16e7
lisadelladucata <lisa.delladucata@gmail.com>
2024-12-17 12:25:51 +0100

    feat:add-third-snack

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,4 +1,9 @@
 const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+
+const incrementedNum = numbers.map(numbers => numbers + 1);
+
+console.log(incrementedNum);
+
 // Risultato: [3, 9, 5, 8, 3, 88]
