5fadddbeb8b4b5c4718b1d39cbe7f0bf95c11994
lisadelladucata <lisa.delladucata@gmail.com>
2024-12-17 12:52:41 +0100

    feat:add-seventh-snack

--- snack-7/script.js
+++ snack-7/script.js
@@ -20,4 +20,8 @@ const students = [
 ];
 
 // Recupera i dati dello studente con id 2
+
+const student = students.find(students => students.id === 2);
+
+console.log(student);
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
