baa4ab0d49098641351153e12df6f08b53e1c508
lisadelladucata <lisa.delladucata@gmail.com>
2024-12-17 12:32:38 +0100

    feat:add-fourth-snack

--- snack-4/script.js
+++ snack-4/script.js
@@ -6,4 +6,8 @@ const posts = [
 ]
 
 // Crea un nuovo array coi nomi di tutti gli autori
+
+const authors = posts.map(posts => posts.author);
+
+console.log(authors);
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
